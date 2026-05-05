8e0758d4bb7e173ffb3e37606e29f2d1519deb66
shiva velmala <shiva1@gmail.com>
2026-05-05 09:53:22 +0530

    adding the changes for delete mapping

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,16 @@ public class ProductController {
 		System.out.println("welcome to the products controller mapping method");
 		return pdao.findAll();
 	}
+	
+	@DeleteMapping("/deleteproduct/{pid}")
+	public String deleteProduct(@PathVariable("pid") int pid) {
+		
+		Products p = pdao.getOne(pid);
+		pdao.delete(p);
+		return "you record deleted successfully";
+		
+	}
+	
 	
 	@GetMapping("/product/{pid}")
 	public Optional<Products> getProduct(@PathVariable("pid") int pid) {
